709aa75faa008f6bf3bb9c48546ab10637f657c3
diptes08 <100475901+diptes08@users.noreply.github.com>
2022-02-26 19:22:11 +0530

    Create HR.java

---
+++ HR.java
@@ -0,0 +1,28 @@
+package com.diptes.io;
+
+import java.util.List;
+
+public class HR extends Employee {
+	public List<Employee> empls;
+
+	public double calculateSalary(Integer id) {
+		double sal=0d;
+		for(Employee e:empls) {
+			if(e.id==id) {
+				sal=e.Salary;
+			}
+		}
+		final float hikePercent = 10;
+		double newSal = sal + (sal * hikePercent) / 100;
+		return newSal;
+	}
+
+	public List<Employee> getEmps() {
+		return empls;
+	}
+
+	public void setEmps(List<Employee> empls) {
+		this.empls = empls;
+	}
+
+}
