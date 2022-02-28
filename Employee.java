4084fe9fb403cbe1285c94674442051c99b94c87
diptes08 <100475901+diptes08@users.noreply.github.com>
2022-02-28 16:46:00 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,54 +1,26 @@
-package com.diptes.io
 
-import java.util.ArrayList;
-import java.util.Scanner;
+		
+package com.junit.testing.io;
 
 public class Employee {
-	public String name;
-	public String add;
-	public double Salary;
-	public int id;
-
-	public Employee() {
-		super();
-
+	public String addCEO(String name) {
+		return name;
 	}
-
-	public Employee(String name, String add, double salary, int id) {
-		
-		this.name = name;
-		this.add = add;
-		Salary = salary;
-		this.id = id;
+	
+	public String addPM(String name) {
+		return name;
 	}
-
-	public static void main(String[] args) {
-		Employee CEO = new Employee("Rajesh Kumar", "Kolkata", 23000000, 100);
-		Employee ProjectManager = new Employee("Rakesh Gopinathan", "Delhi", 75000, 200);
-		Employee TeamLeader = new Employee("Bala", "Chandigarh", 65000, 300);
-		HR hr = new HR();
-		ArrayList<Employee> emp = new ArrayList<>();
-		emp.add(CEO);
-		emp.add(ProjectManager);
-		emp.add(TeamLeader);
-		hr.setEmps(emp);
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Please enter Emp id to  check for Employee details:");
-		int id=sc.nextInt();
-		for(Employee e:emp) {
-			if(e.id==id) {
-				System.out.println("Emp Name:"+e.name);
-				System.out.println("Old Salary:"+e.Salary);
-				System.out.println("location:"+e.add);
-				System.out.println("new Salary:"+hr.calculateSalary(id));
-				
-			}
-		}
+	
+	public String addLead(String name) {
+		return name;
+	}
+	
+	public double Salary(double oSalary) {
 		
+		double salHikePer =10;
+		double nSalary =((oSalary*7.5)/100)+(oSalary);
+		return nSalary;
 		
-		
-		
-
 	}
 
 }
